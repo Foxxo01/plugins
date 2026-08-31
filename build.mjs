b1fcee8c60566e648bc3ade3b288381b28a0a179
Foxxo01 <162824544+Foxxo01@users.noreply.github.com>
2026-08-31 18:38:15 +0300

    Update build.mjs

--- build.mjs
+++ build.mjs
@@ -7,7 +7,6 @@ import esbuild from "rollup-plugin-esbuild";
 import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import alias from "@rollup/plugin-alias";
-import swc from "@swc/core";
 import { fileURLToPath } from "url";
 
 const extensions = [".js", ".jsx", ".mjs", ".ts", ".tsx", ".cts", ".mts"];
@@ -25,36 +24,14 @@ const plugins = [
     }),
     nodeResolve({ extensions }),
     commonjs(),
-    {
-        name: "swc",
-        async transform(code, id) {
-            const ext = extname(id);
-            if (!extensions.includes(ext)) return null;
-
-            const ts = ext.includes("ts");
-            const tsx = ts ? ext.endsWith("x") : undefined;
-            const jsx = !ts ? ext.endsWith("x") : undefined;
-
-            const result = await swc.transform(code, {
-                filename: id,
-                jsc: {
-                    externalHelpers: true,
-                    parser: {
-                        syntax: ts ? "typescript" : "ecmascript",
-                        tsx,
-                        jsx,
-                    },
-                    target: "es2022",
-                },
-                module: {
-                    type: "es6"
-                },
-                isModule: true
-            });
-            return result.code;
-        },
-    },
-    esbuild({ minify: true }),
+    esbuild({ 
+        minify: true,
+        target: "es2022",
+        loaders: {
+            ".ts": "ts",
+            ".tsx": "tsx"
+        }
+    }),
 ];
 
 for (let plug of await readdir("./plugins")) {
