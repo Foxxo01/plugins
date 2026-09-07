81c878b436540d0d2b6f4d7cbb2f921778791a30
Foxxo01 <162824544+Foxxo01@users.noreply.github.com>
2026-09-07 16:41:34 +0300

    Update index.ts

--- plugins/ShowMoreBadges/src/index.ts
+++ plugins/ShowMoreBadges/src/index.ts
@@ -1,31 +1,42 @@
-import { findByProps } from "@vendetta/metro";
+import { findByProps, findByStoreName } from "@vendetta/metro";
 
 const unpatches: Array<() => void> = [];
 
 export default {
   onLoad: () => {
     try {
-      
       const BadgeUtils = findByProps("getBadges", "MAX_BADGES") || findByProps("MAX_BADGES_TO_DISPLAY");
-      
       if (BadgeUtils) {
-        if (BadgeUtils.MAX_BADGES !== undefined) {
-          const origMax = BadgeUtils.MAX_BADGES;
-          BadgeUtils.MAX_BADGES = 99;
-          unpatches.push(() => { BadgeUtils.MAX_BADGES = origMax; });
-        }
+        if (BadgeUtils.MAX_BADGES !== undefined) BadgeUtils.MAX_BADGES = 99;
+        if (BadgeUtils.MAX_BADGES_TO_DISPLAY !== undefined) BadgeUtils.MAX_BADGES_TO_DISPLAY = 99;
 
-        if (BadgeUtils.MAX_BADGES_TO_DISPLAY !== undefined) {
-          const origMaxToDisplay = BadgeUtils.MAX_BADGES_TO_DISPLAY;
-          BadgeUtils.MAX_BADGES_TO_DISPLAY = 99;
-          unpatches.push(() => { BadgeUtils.MAX_BADGES_TO_DISPLAY = origMaxToDisplay; });
+        if (typeof BadgeUtils.getBadges === "function") {
+          const origGetBadges = BadgeUtils.getBadges;
+          BadgeUtils.getBadges = function (...args: any[]) {
+            return origGetBadges.apply(this, args);
+          };
+          unpatches.push(() => { BadgeUtils.getBadges = origGetBadges; });
+        }
+      }
+
+      const UserProfileStore = findByStoreName("UserProfileStore") || findByProps("getUserProfile");
+      if (UserProfileStore) {
+        const origGetProfile = UserProfileStore.getUserProfile;
+        if (typeof origGetProfile === "function") {
+          UserProfileStore.getUserProfile = function (...args: any[]) {
+            const profile = origGetProfile.apply(this, args);
+            if (profile && Array.isArray(profile.badges)) {
+              profile.traitBadges = profile.badges;
+            }
+            return profile;
+          };
+          unpatches.push(() => { UserProfileStore.getUserProfile = origGetProfile; });
         }
       }
     } catch (e) {}
   },
 
   onUnload: () => {
-    
     unpatches.forEach((u) => {
       try { u(); } catch (e) {}
     });
