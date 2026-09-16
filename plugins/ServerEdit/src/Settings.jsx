c92cb6eccca8eb941153375d02f1fdffe939cc4e
Foxxo01 <162824544+Foxxo01@users.noreply.github.com>
2026-09-16 19:37:36 +0300

    Update text for clarity in Settings component

--- plugins/ServerEdit/src/Settings.jsx
+++ plugins/ServerEdit/src/Settings.jsx
@@ -6,7 +6,7 @@ const { FormText } = Forms;
 export default function Settings() {
   return (
     <FormText style={{ padding: 15, color: "#aaa" }}>
-      Eklenti aktif. Ekrandaki herhangi bir yazıya basılı tutarak içeriğini doğrudan değiştirebilirsin.
+      Eklenti aktif. Ekrandaki herhangi bir yazıya uzun basarak metni düzenlenebilir yapabilirsin.
     </FormText>
   );
 }
