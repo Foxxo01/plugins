52f123a18b2257e1e01e18b265e7a2c1b7d5aa04
Foxxo01 <162824544+Foxxo01@users.noreply.github.com>
2026-09-16 18:56:21 +0300

    Update Settings.jsx

--- plugins/ServerEdit/src/Settings.jsx
+++ plugins/ServerEdit/src/Settings.jsx
@@ -1,15 +1,18 @@
-import { React, storage } from "@vendetta/metro/common";
+import { React } from "@vendetta/metro/common";
+import { storage } from "@vendetta/plugin";
 import { Forms } from "@vendetta/ui/components";
 
 const { FormInput } = Forms;
 
-export default () => {
+export default function Settings() {
   return (
     <FormInput
       label="Target Guild ID"
       value={storage.targetGuildId ?? ""}
-      onChange={(v) => (storage.targetGuildId = v)}
+      onChange={(v) => {
+        storage.targetGuildId = v;
+      }}
       placeholder="Sunucu ID'si girin"
     />
   );
-};
+}
