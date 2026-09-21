6efd53d517e94a60d3cc78587b6f6e4f36825850
Foxxo01 <162824544+Foxxo01@users.noreply.github.com>
2026-09-21 16:30:44 +0300

    Rename nitroFireBadge id from 'nitro_fire' to 'nitro'

--- plugins/staff/src/index.ts
+++ plugins/staff/src/index.ts
@@ -110,7 +110,7 @@ export default {
                   };
 
                   const nitroFireBadge = {
-                    id: "nitro_fire",
+                    id: "nitro",
                     key: "nitro_fire",
                     description: "Nitro Fire",
                     icon: "cff7119d4417261c3f52fde8a94ba8e5",
